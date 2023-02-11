d13c69f88799a6f785d5e3daabbabdec6e612254
natakozlova <natabalabanova555@yandex.ru>
2023-02-11 14:05:19 -0800

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int primaryBalance = 500;
+        int addition = 1200;
+        int bonus = 0;
+
+        if (addition >1000) {
+            bonus = addition / 100;
+        }
+
+        int finalBalance = primaryBalance + addition + bonus;
+
+        System.out.println("bonus: " + bonus + ", finalBalance: " + finalBalance);
     }
 }
